80d68c6fccdbfadf525dc4b178535b1fb0bcb1a1
Tulebaeva Marzhan <241398@astanait.edu.kz>
2026-01-21 15:20:50 +0500

    Practice 11

---
+++ app.js
@@ -0,0 +1,112 @@
+require('dotenv').config();
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URI;
+
+const client = new MongoClient(MONGO_URL);
+
+let products;
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.use(express.json());
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected");
+
+    const db = client.db("shop");
+    products = db.collection("products");
+
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+startServer();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Shop API</h1>
+    <ul>
+      <li><a href="/api/products">/api/products</a></li>
+      <li><a href="/api/products/1">/api/products/1</a></li>
+    </ul>
+  `);
+});
+
+app.get("/api/products", async (req, res) => {
+  const { category, minPrice, sort, fields } = req.query;
+
+  const filter = {};
+  if (category) {
+    filter.category = category;
+  }
+  if (minPrice) {
+    filter.price = { $gte: Number(minPrice) };
+  }
+
+  const sortOption = {};
+  if (sort === "price") {
+    sortOption.price = 1;
+  }
+
+  let projection = null;
+  if (fields) {
+    projection = {};
+    fields.split(",").forEach((field) => {
+      projection[field] = 1;
+    });
+    projection._id = 0;
+  }
+
+  let query = products.find(filter);
+
+  if (sort) {
+    query = query.sort(sortOption);
+  }
+
+  if (projection) {
+    query = query.project(projection);
+  }
+
+  const productsList = await query.toArray();
+
+  res.json({
+    count: productsList.length,
+    products: productsList,
+  });
+});
+
+app.post("/api/products", async (req, res) => {
+  const { name, price, category } = req.body;
+
+  if (!name || price === undefined || !category) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  const result = await products.insertOne({
+    name,
+    price,
+    category,
+  });
+
+  res.status(201).json({
+    id: result.insertedId,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "API endpoint not found" });
+});
